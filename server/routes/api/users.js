004279802be6dbb26a8d22b9f022eb032e339e11
SpikeBasel <spikerine@gmail.com>
2023-01-10 16:27:08 +0500

    Added RESTful POST, DELETE path for user

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -10,6 +10,25 @@ router.get('/', async (req, res) => {
   return;
 });
 
+// Add User
+router.post('/', async (req, res) => {
+  const users = await loadUsersCollection();
+  await users.insertOne({
+    email: req.body.email,
+    password: req.body.password,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+  return;
+});
+
+// Delete User
+router.delete('/:id', async (req, res) => {
+  const users = await loadUsersCollection();
+  await users.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send({});
+  return;
+});
 
 async function loadUsersCollection() {
   const client = await mongodb.MongoClient.connect(
